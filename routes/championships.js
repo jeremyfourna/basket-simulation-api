819bc646af0448c6861ec791e5ccbd2c7ca50c54
jeremyfourna <jeremyfourna@gmail.com>
2017-11-07 21:43:21 +0100

    WIP on post

--- routes/championships.js
+++ routes/championships.js
@@ -1,15 +1,28 @@
+const R = require('ramda');
 const express = require('express');
 const router = express.Router();
 const { generateChampionship } = require('basket-simulation-champ');
 const monk = require('monk');
 const db = monk('localhost:27017/test');
 
-const champ = db.get('championships');
+const champs = db.get('championships');
+const clubs = db.get('clubs');
 
 
 router.get('/:id', (req, res, next) => {
-  champ.findOne({ _id: req.params.id }).then((championships) => {
+  champs.findOne({ _id: R.path(['params', 'id'], req) }).then(championships => {
     res.json(championships);
+  });
+});
+
+router.post('/', (req, res) => {
+  const params = R.prop('body', req);
+  const newChampionship = generateChampionship(R.prop('style', params), R.prop('level', params), R.prop('nbClubs', params));
+
+  clubs.insert(R.tail(newChampionship)).then(createdClubs => {
+    champs.insert(R.assoc('clubs', createdClubs, R.head(newChampionship))).then(createdChamp => {
+      res.status(201).json({ _id: createdChamp });
+    });
   });
 });
 
