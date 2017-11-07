35649bf7026c3079787818516985dccc480d18cd
Jeremy Fourna <jeremyfourna@gmail.com>
2017-11-07 16:22:23 +0100

    Add monk

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- routes/championships.js
+++ routes/championships.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const { generateChampionship } = require('basket-simulation-champ');
+const monk = require('monk');
+const db = monk('localhost:27017/test');
+
+const champ = db.get('championships');
 
 
 router.get('/:id', (req, res, next) => {
-  res.json(generateChampionship());
+  champ.findOne(monk.id(req.params.id)).then((championships) => {
+    console.log(championships);
+    res.json(championships);
+  });
 });
 
 module.exports = router;
