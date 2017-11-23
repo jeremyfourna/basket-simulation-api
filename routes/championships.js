d2867052935ef7c034351e92ad40a7155eedfeac
Jeremy Fourna <jeremyfourna@gmail.com>
2017-11-23 18:10:12 +0100

    WIP for refactor

--- routes/championships.js
+++ routes/championships.js
@@ -1,61 +1,98 @@
 const R = require('ramda');
+const Ajv = require('ajv');
+const ajv = new Ajv({ allErrors: true });
 const express = require('express');
 const router = express.Router();
 const { generateChampionship } = require('basket-simulation-champ');
 const monk = require('monk');
 const db = monk('localhost:27017/test');
 const {
-  isValidId,
-  findOneFromCollection
+  findOneFromCollection,
+  badArguments,
+  insertOneInCollection
 } = require('./utils');
+const {
+  postChampionshipSchema,
+  getOneChampionshipSchema
+} = require('./schemas');
 
 const champs = db.get('championships');
 const clubs = db.get('clubs');
 
 
+///// GET 1 championship ///////////////////////////////////////////////////////////////
+
+// Function to validate the request params
+const validateGetChamp = ajv.compile(getOneChampionshipSchema);
+// Retrieve a given championship
 router.get('/:_id', findOneFromCollection(
   ['_id'],
-  isValidId,
-  'Champ id must have a length of 24.',
+  validateGetChamp,
   champs
 ));
 
+////////////////////////////////////////////////////////////////////////////////////////
 
-router.post('/', (req, res) => {
-  function createClubForChamp(clubsList, champId, callback) {
-    // Add championship Id inside each club
-    const newClubs = R.map(R.assoc('champId', champId), clubsList);
-    // Insert the clubs inside the DB
-    clubs.insert(newClubs).then(createdClubs => {
-      // Retrieve the clubs Id after the insert
-      const clubsId = R.map(R.prop('_id'), createdClubs);
-      // Call the callback to update the championship
-      // with the clubs id
-      return callback(champId, clubsId);
-    });
-  }
+///// POST a new championship //////////////////////////////////////////////////////////
 
-  function updateChampWithClubsId(champId, clubsId) {
-    // After creating the clubs, link them inside the championship
-    champs.update(champId, { $set: { clubs: clubsId } });
-    return champId;
-  }
+function createClubForChamp(clubsList, champId, callback) {
+  // Add championship Id inside each club
+  const newClubs = R.map(R.assoc('champId', champId), clubsList);
+  // Insert the clubs inside the DB
+  clubs.insert(newClubs).then(createdClubs => {
+    // Retrieve the clubs Id after the insert
+    const clubsId = R.map(R.prop('_id'), createdClubs);
+    // Call the callback to update the championship
+    // with the clubs id
+    return callback(champId, clubsId);
+  });
+}
 
-  const params = R.prop('body', req);
+function updateChampWithClubsId(champId, clubsId) {
+  // After creating the clubs, link them inside the championship
+  champs.update(champId, { $set: { clubs: clubsId } });
+  return champId;
+}
+
+function createChampionship(response, params) {
   const newChampionship = generateChampionship(
     R.prop('style', params),
     R.prop('level', params),
     R.prop('nbClubs', params)
   );
+  const championship = R.head(newChampionship);
+  const clubsInsideChamp = R.last(newChampionship);
+
   // Insert the championship
-  champs.insert(R.head(newChampionship)).then(createdChamp => {
-    // Retrieve the championship Id
-    const champId = R.prop('_id', createdChamp);
-    // Respond to the API call
-    res.status(201).json({ _id: champId });
-    // Create the clubs for the championship
-    createClubForChamp(R.last(newChampionship), champId, updateChampWithClubsId);
-  });
-});
+  return insertOneInCollection(
+    response,
+    champs,
+    championship, [createClubForChampCurry(clubsInsideChamp), updateChampWithClubsIdCurry]
+  );
+}
+
+function manageChampionshipCreation(request, response) {
+  const createClubForChampCurry = R.curry(createClubForChamp);
+  const updateChampWithClubsIdCurry = R.curry(updateChampWithClubsId);
+
+  // Function to validate the request params
+  const validatePostChamp = ajv.compile(postChampionshipSchema);
+  // Retrieve the championship Id from the request
+  const data = R.prop('body', request);
+  // Validation
+  const isDataValid = validatePostChamp(data);
+
+  return R.ifElse(
+    R.equals(true),
+    () => createChampionship(response, data),
+    () => badArguments(R.prop('errors', validatePostChamp), response)
+  )(isDataValid);
+}
+
+
+// Create a new championship
+router.post('/', manageChampionshipCreation);
+
+////////////////////////////////////////////////////////////////////////////////////////
 
 module.exports = router;
--- routes/utils.js
+++ routes/utils.js
@@ -1,11 +1,5 @@
 const R = require('ramda');
 
-function isValidId(id) {
-  const identifier = R.prop('_id', id);
-  return R.equals(R.length(identifier), 24);
-}
-
-
 function findOne(collection, query, response) {
   function resultForFind(result) {
     return R.ifElse(
@@ -21,14 +15,8 @@ function badArguments(message, response) {
   return response.status(400).json({ message });
 }
 
-function findOneFromCollection(
-  paramsToRetrieve,
-  validation,
-  errorMessage,
-  collection,
-  request,
-  response,
-  next) {
+
+function findOneFromCollection(paramsToRetrieve, validation, collection, request, response, next) {
   // Retrieve the championship Id from the request
   const data = R.pickAll(paramsToRetrieve, R.prop('params', request));
   // Validation
@@ -37,9 +25,50 @@ function findOneFromCollection(
   return R.ifElse(
     R.equals(true),
     () => findOne(collection, data, response),
-    () => badArguments(errorMessage, response)
+    () => badArguments(R.prop('errors', validation), response)
   )(isDataValid);
 }
 
-exports.isValidId = isValidId;
+function insertOneInCollection(response, collection, data, callbacks) {
+  function getIdentifier(createdDocument) {
+    return R.prop('_id', createdDocument);
+  }
+
+  function respondToInsertOne(createdDocument) {
+    // Respond to the API call
+    return response.status(201).json({ _id: getIdentifier(createdDocument) });
+  }
+
+  return collection.insert(data).then(createdDocument => {
+    callbackShouldContinue(callbacks, getIdentifier(createdDocument));
+    return respondToInsertOne(createdDocument);
+  });
+}
+
+function insertManyInCollection(response, collection, data, callbacks) {
+  function getIdentifier(createdDocument) {
+    return R.prop('_id', createdDocument);
+  }
+
+  function respondToInsertOne(createdDocument) {
+    // Respond to the API call
+    return response.status(201).json({ _id: getIdentifier(createdDocument) });
+  }
+
+  return collection.insert(data).then(createdDocument => {
+    callbackShouldContinue(callbacks, getIdentifier(createdDocument));
+    return respondToInsertOne(createdDocument);
+  });
+}
+
+function callbackShouldContinue(callbacks, data) {
+  return R.ifElse(
+    R.equals(0),
+    R.T,
+    () => R.head(callbacks)(data, R.tail(callbacks))
+  )(R.length(callbacks));
+}
+
 exports.findOneFromCollection = R.curry(findOneFromCollection);
+exports.insertOneInCollection = R.curry(insertOneInCollection);
+exports.badArguments = R.curry(badArguments);
