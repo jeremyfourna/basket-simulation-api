38632c58759834554e646d0bfadece18ae38bb22
jeremyfourna <jeremyfourna@gmail.com>
2017-11-28 21:52:47 +0100

    Start adding test automation

--- routes/championships.js
+++ routes/championships.js
@@ -103,4 +103,62 @@ router.post('/', postOneChampionship);
 
 ////////////////////////////////////////////////////////////////////////////////////////
 
+///// DELETE a new championship ////////////////////////////////////////////////////////
+
+// deleteOneChampionship io io io -> io
+function deleteOneChampionship(request, response) {
+  function createChampionship(params) {
+    const prop = R.prop(R.__, params);
+    const newChampionship = generateChampionship(
+      prop('style'),
+      prop('level'),
+      prop('nbClubs')
+    );
+    const championship = R.head(newChampionship);
+    const clubsInsideChamp = R.last(newChampionship);
+    const postManyClubsForChampC = R.curry(postManyClubsForChamp);
+
+    return insert(
+      champs,
+      championship,
+      postManyClubsForChampC(response, clubsInsideChamp)
+    );
+  }
+
+  function postManyClubsForChamp(response, clubsList, championship) {
+    function updateChampWithClubsId(champId, clubsId) {
+      const listOfClubsId = R.map(R.prop('_id'), clubsId);
+      const updateQuery = {
+        '$set': {
+          clubs: listOfClubsId
+        }
+      };
+
+      return updateOne(champs, champId, updateQuery);
+    }
+    const champId = R.prop('_id', championship);
+    const queryChampId = R.pickAll(['_id'], championship);
+    const newClubs = R.map(R.assoc('champId', champId), clubsList);
+    const updateChampWithClubsIdC = R.curry(updateChampWithClubsId);
+
+    insert(clubs, newClubs, updateChampWithClubsIdC(queryChampId));
+
+    return respondToPOSTCall(response, championship);
+  }
+
+  const params = paramsForPOSTCall(['style', 'level', 'nbClubs'], request);
+  const isRequestValid = validateRequest(postChampionshipSchema, params);
+
+  return R.ifElse(
+    R.equals(true),
+    () => createChampionship(params),
+    () => badArguments(response, R.last(isRequestValid))
+  )(R.head(isRequestValid));
+}
+
+// Create a new championship
+router.delete('/:_id', deleteOneChampionship);
+
+////////////////////////////////////////////////////////////////////////////////////////
+
 module.exports = router;
